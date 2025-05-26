e6072765af9cf0958afa4beb456ce8e64e92725b
raquel <raquelwendt24@gmail.com>
2025-05-26 17:33:50 -0300

    ajuste de cliente

--- z_Cliente/adicionar_cliente.php
+++ z_Cliente/adicionar_cliente.php
@@ -21,7 +21,7 @@ endif;
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>RAtendimento</title>
+	<title>Sistema de Atendimento</title>
 	<!-- CSS do Bootstrap 5 -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
 		integrity="sha384-..." crossorigin="anonymous">
@@ -55,19 +55,18 @@ endif;
 	<div class="row mt-4">
 		<div class="col-12 col-md-8 offset-md-2">
 			<div class="p-1 mb-3 bg-dark text-white rounded border border-light">
-				<h3 class="text-center">Novo Atendente</h3>
+				<h3 class="text-center">Novo Cliente</h3>
 			</div>
 			<form action="crud_Cliente/create_cliente.php" method="POST">
 				<div class="rounded border border-secondary p-3">
 					<div class="input-group input-group mb-3">
 						<span class="input-group-text" id="inputGroup-sizing-sm" style="min-width: 10%;">Nome</span>
 						<input type="text" name="nome" id="nome" class="form-control" placeholder="Digite o nome"
-							minlength="45" required>
+							minlength="20" required>
 					</div>
 					<div class="input-group input-group mb-3">
 						<span class="input-group-text" id="inputGroup-sizing-sm" style="min-width: 10%;">CNPJ</span>
-						<input type="text" name="cnpj" id="cpf" class="form-control" placeholder="Ex.: 00.000.000/0000-00"
-							minlength="18" required>
+						<input type="text" name="cnpj" id="cnpj" class="form-control" placeholder="Ex.: 00.000.000/0000-00" required>
 					</div>
 					<div class="input-group input-group mb-3">
 						<span class="input-group-text" id="inputGroup-sizing-sm" style="min-width: 10%;">Telefone</span>
@@ -81,10 +80,9 @@ endif;
 					</div>
 				</div>
 
-				<button type="submit" name="btn-cadastrar" class="btn btn-secondary mt-3 float-end"><i
+				<button type="submit" name="btn-cadastrar" class="btn btn-info mt-3 float-end"><i
 						class="bi bi-plus"></i>Cadastrar</button>
-				<a href="cliente.php" class="btn btn-success mt-3 float-end me-2"><i class="fas fa-list"></i> Lista de
-					Clientes</a>
+				<a href="cliente.php" class="btn btn-success mt-3 float-end me-2"></i>Voltar</a>
 
 			</form>
 		</div>
